09d769d21c1002cd351a1ad395e8da95c9941b9b
Martin Work PC <15941171+nurrix@users.noreply.github.com>
2026-01-10 22:01:40 +0100

    Added ESP32 code for generating a signal

--- files/esp32_signal_generator/esp32_signal_generator.ino
+++ files/esp32_signal_generator/esp32_signal_generator.ino
@@ -1,14 +1,13 @@
 void setup() {
   // put your setup code here, to run once:
-
-
-
+  Serial.begin(115200);
+  delay(400);
 }
-float t= 0.0;
-
+float counter = 0;
 void loop() {
   // put your main code here, to run repeatedly:
-  float dt = 1.0/50.0;
-  t+=dt;
-  printf("%.3f\n", sin(t));
+  float dt = 0.1* PI;
+  Serial.printf("%.2f\n",sin(counter* PI)+sin(counter* PI*0.27));
+  counter+=dt;
+  delay(100);
 }
